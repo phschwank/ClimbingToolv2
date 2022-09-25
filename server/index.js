58b08018db688a3ff3a1280994a6c0c5c8d36fea
Philippe Schwank <philippe.schwank@gmail.com>
2022-09-25 16:26:40 +0200

    server/client: added session packages

--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const pool = require('./db');
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
+
+app.use(session({
+    key: "userId",
+    secret: "I am a secret for the session.", // TODO: change this
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60 * 60 * 24, // 1 day
+    },
+}));
+
 
 // ROUTES //
 
