f68bb7adf93d6a1920df5fcbddd81ff605c92739
Philippe Schwank <philippe.schwank@gmail.com>
2022-11-13 19:36:22 +0100

    server: change logoutController.js

--- server/controllers/logoutController.js
+++ server/controllers/logoutController.js
@@ -1,26 +1,32 @@
-const User = require('../model/User');
+const connectDB = require('../config/dbConn');
 
 const handleLogout = async (req, res) => {
     // On client, also delete the accessToken
 
     const cookies = req.cookies;
+    // check if refresh token exists
     if (!cookies?.jwt) return res.sendStatus(204); //No content
     const refreshToken = cookies.jwt;
 
     // Is refreshToken in db?
-    const foundUser = await User.findOne({ refreshToken }).exec();
-    if (!foundUser) {
+    try {
+        const pool = await connectDB();
+        // check if refresh token exists in db
+        const user = await pool.query("SELECT * FROM users WHERE refresh_token = $1", [refreshToken]);
+        // check if user exists
+        if (user.rows.length === 0) {
+            res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+            return res.sendStatus(204);
+        }
+
+        // delete refreshToken in db
+        await pool.query("UPDATE users SET refresh_token = NULL WHERE refresh_token = $1", [refreshToken]);
+        
         res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-        return res.sendStatus(204);
+        res.sendStatus(204);
+    } catch (err) {
+        res.status(500).json({ 'message': err.message });
     }
-
-    // Delete refreshToken in db
-    foundUser.refreshToken = '';
-    const result = await foundUser.save();
-    console.log(result);
-
-    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-    res.sendStatus(204);
 }
 
 module.exports = { handleLogout }
