e3bcb9524885026c5684a5cc9e266619d69c0b38
Philippe Schwank <philippe.schwank@gmail.com>
2022-11-13 19:23:07 +0100

    server: changed usersController.js

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,28 +1,53 @@
 const User = require('../model/User');
+const connectDB = require('../config/dbConn');
 
 const getAllUsers = async (req, res) => {
-    const users = await User.find();
-    if (!users) return res.status(204).json({ 'message': 'No users found' });
-    res.json(users);
+    const pool = await connectDB();
+    try {
+        const users = await pool.query("SELECT * FROM users");
+        if (users.rows.length === 0) {
+            return res.status(204).json({ 'message': 'No users found' }); // no content
+        }
+        res.json(users.rows);
+    }
+    catch (err) {
+        res.status(500).json({ 'message': err.message });
+    }
 }
 
 const deleteUser = async (req, res) => {
+        // check if necessary fields are filled
     if (!req?.body?.id) return res.status(400).json({ "message": 'User ID required' });
-    const user = await User.findOne({ _id: req.body.id }).exec();
-    if (!user) {
-        return res.status(204).json({ 'message': `User ID ${req.body.id} not found` });
+
+    const pool  = await connectDB();
+    try {
+        const user = await pool.query("SELECT * FROM users WHERE id = $1", [req.body.id]);
+        if (user.rows.length === 0) {
+            return res.status(204).json({ 'message': `User ID ${req.body.id} not found` });
+        }
+        await pool.query("DELETE FROM users WHERE id = $1", [req.body.id]);
+        res.json({ 'message': `User ID ${req.body.id} deleted` });
     }
-    const result = await user.deleteOne({ _id: req.body.id });
-    res.json(result);
+    catch (err) {
+        res.status(500).json({ 'message': err.message });
+    }
 }
 
 const getUser = async (req, res) => {
+    // check if necessary fields are filled
     if (!req?.params?.id) return res.status(400).json({ "message": 'User ID required' });
-    const user = await User.findOne({ _id: req.params.id }).exec();
-    if (!user) {
-        return res.status(204).json({ 'message': `User ID ${req.params.id} not found` });
+
+    const pool  = await connectDB();
+    try {
+        const user = await pool.query("SELECT * FROM users WHERE id = $1", [req.params.id]);
+        if (user.rows.length === 0) {
+            return res.status(204).json({ 'message': `User ID ${req.params.id} not found` });
+        }
+        res.json(user.rows[0]);
     }
-    res.json(user);
+    catch (err) {
+        res.status(500).json({ 'message': err.message });
+    }
 }
 
 module.exports = {
