332c5679c3c6e98e7ea13014011dda785ba784e0
Philippe Schwank <philippe.schwank@gmail.com>
2022-11-13 19:14:48 +0100

    server: changed refreshTokenController.js

--- server/controllers/refreshTokenController.js
+++ server/controllers/refreshTokenController.js
@@ -1,33 +1,45 @@
-const User = require('../model/User');
 const jwt = require('jsonwebtoken');
+const connectDB = require('../config/dbConn');
 
 const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
-    if (!cookies?.jwt) return res.sendStatus(401);
+
+    // check if refresh token exists
+    if (!cookies?.jwt) return res.sendStatus(401); // no refresh token: unauthorized
     const refreshToken = cookies.jwt;
 
-    const foundUser = await User.findOne({ refreshToken }).exec();
-    if (!foundUser) return res.sendStatus(403); //Forbidden 
-    // evaluate jwt 
-    jwt.verify(
-        refreshToken,
-        process.env.REFRESH_TOKEN_SECRET,
-        (err, decoded) => {
-            if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
-            const roles = Object.values(foundUser.roles);
-            const accessToken = jwt.sign(
-                {
-                    "UserInfo": {
-                        "username": decoded.username,
-                        "roles": roles
-                    }
-                },
-                process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '10s' }
-            );
-            res.json({ roles, accessToken })
+    try {
+        const pool = await connectDB();
+        // check if refresh token exists in db
+        const user = await pool.query("SELECT * FROM users WHERE refresh_token = $1", [refreshToken]);
+        // check if user exists
+        if (user.rows.length === 0) {
+            return res.sendStatus(403); // forbidden
         }
-    );
+
+        // evaluate jwt
+        jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+            (err, decoded) => {
+                if (err || user.rows[0].username !== decoded.username) return res.sendStatus(403); // forbidden
+                const roles = user.rows[0].roles.split(',');
+                const accessToken = jwt.sign(
+                    {
+                        "UserInfo": {
+                            "username": decoded.username,
+                            "roles": roles
+                        }
+                    },
+                    process.env.ACCESS_TOKEN_SECRET,
+                    { expiresIn: '10s' }
+                );
+                res.json({ roles, accessToken });
+            }
+        );
+    } catch (err) {
+        res.status(500).json({ 'message': err.message });
+    }
 }
 
 module.exports = { handleRefreshToken }
